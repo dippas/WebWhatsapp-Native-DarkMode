502a1d6d8ce5d44bdb4f9352ae7a8c7e4918908f
Fábio Serpa <fabio.serpa@fever.pt>
2020-06-08 01:19:01 +0100

    ADD js files

---
+++ js/userscript/webwhatsappnativedarkmode.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        WebWhatsappNativeDarkMode
+// @author      dippas
+// @namespace   https://github.com/dippas/
+// @homepage    https://github.com/dippas/
+// @description Add native darkmode to web whatsapp
+// @version     1.0
+// @match       https://web.whatsapp.com/*
+// @grant       none
+// ==/UserScript==
+
+const darkMode = {
+	el: {
+		body: document.body
+	},
+
+	init() {
+		window.addEventListener('load', () => this.el.body.classList.add('dark'))
+	}
+}
+darkMode.init()
